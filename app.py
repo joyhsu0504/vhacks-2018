9400d5efb5a093013faf75cbf779fa456a54e2a4
Joy Hsu <joycj@stanford.edu>
2018-03-10 02:21:41 +0100

    dialog flow

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def root():
 	print('let go')
 	data = request.data
 	print(data)
-	action = data[0][6]['intentName']
+	action = data[3][6]['intentName']
 	print('start')
 	print(action)
 	if action == 'Field-question':
